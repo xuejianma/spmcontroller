b1a16656596a4ba52f920523b2b1f2f86fa8f39f
xuejianma <xuejian.ma@gmail.com>
2022-11-22 18:04:19 -0600

    Comments about thorlabs_apt crashing program without error message potentially.

--- lib/ndfiltercontroller.py
+++ lib/ndfiltercontroller.py
@@ -4,8 +4,8 @@ thorlab_apt package from https://github.com/qpit/thorlabs_apt
 try:
     # thorlab_apt package from https://github.com/qpit/thorlabs_apt.
     # Sometimes this will crash the program without giving any error
-    # message even with print(e). Rerun thorlab's Kinesis program 
-    # and close it can solve the problem.
+    # message even with print(e). Starting thorlab's Kinesis program 
+    # and closing it can solve the problem.
     import thorlabs_apt as apt
 except Exception as e:
     print(e)
